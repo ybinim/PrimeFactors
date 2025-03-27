160f358022e6b79eccdde6acc69bd0def7fbb915
ImYongbin <lyb0203@gmail.com>
2025-03-27 14:31:34 +0900

    [feature] 37

--- Project22/prime_factors.cpp
+++ Project22/prime_factors.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 class PrimeFactors {
 public:
-	vector<int> of(int i) {
+	vector<int> of(int number) {
 		vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 
--- Project22/prime_factors_test.cpp
+++ Project22/prime_factors_test.cpp
@@ -10,6 +10,12 @@ TEST(PrimeFactors, Of1) {
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
+TEST(PrimeFactors, Of2) {
+	PrimeFactors prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
