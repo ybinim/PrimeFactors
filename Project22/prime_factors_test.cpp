6f14d08a7f8bc910ddd79f47277c1a2a2759e94a
ImYongbin <lyb0203@gmail.com>
2025-03-27 14:34:00 +0900

    [Refactoring] 38

--- Project22/prime_factors_test.cpp
+++ Project22/prime_factors_test.cpp
@@ -4,15 +4,20 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFactorsFixture : public ::testing::Test {
+public:
 	PrimeFactors prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+
+TEST_F(PrimeFactorsFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
 
-TEST(PrimeFactors, Of2) {
-	PrimeFactors prime_factor;
-	vector<int> expected = {2};
+TEST_F(PrimeFactorsFixture, Of2) {
+	expected = {2};
 	EXPECT_EQ(expected, prime_factor.of(2));
 }
 
